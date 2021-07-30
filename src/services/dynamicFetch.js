501fa4db4ac07fcef21885ee483d25a335da267e
Gabriel Simek <gabrielsimek@outlook.com>
2021-07-29 22:09:24 -0700

    check for error on fetch

--- src/services/dynamicFetch.js
+++ src/services/dynamicFetch.js
@@ -1,17 +1,23 @@
 const makeRequest = async (url, method, body) => {
-  let res;
-  if(body){
+  try {
+    let res;
+    if(body){
+      res = await fetch(url, {
+        method,
+        body
+      });
+    }
     res = await fetch(url, {
-      method,
-      body
+      method
     });
+  
+    const json = await res.json();
+    return json;
+    
+  } catch(error) {
+    if(error) return error.message;
   }
-  res = await fetch(url, {
-    method
-  });
 
-  const json = await res.json();
-  return json;
 };
 
 export default makeRequest;
